785995b10e023fe1f1e3dc6f4bb147704351db41
Linus <linushakansson89@gmail.com>
2018-06-27 12:19:04 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,10 @@
 import requests
 from flask import Flask
 from flask import render_template
-from flask_sqlalchemy import SQLAlchemy
-from config import Configuration
 
 # create Flask app
 app = Flask(__name__)
 app.config.from_object(Configuration)
-db = SQLAlchemy(app)
 
 # GET request to ip.jsontest.com
 def home():
@@ -16,20 +13,7 @@ def home():
 def rest_request_example():
     print (requests.get("http://ip.jsontest.com/").text)
 
-def read_db_SQL_example():
-    conn = db.get_engine().connect()
-    sql = "SELECT * FROM SampleTable"
-    results = conn.execute(sql)
-    rows = ""
-    for row in results:
-        rows = rows + ','.join(row) + "<br/>"
-    print(rows)
-	
 rest_request_example()
-try:
-	read_db_SQL_example()
-except:
-	print ('Could not connect to database')
 	
 if __name__ == '__main__':
     app.run(host=app.config['HOST'], port=app.config['PORT'])
