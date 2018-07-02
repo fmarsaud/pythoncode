96c6454a71f6f967d0bc27e57dd8ecf609366fa1
fmarsaud <fmarsaud@redhat.com>
2018-07-02 15:00:12 +0200

    Update app.py

--- app.py
+++ app.py
@@ -13,7 +13,11 @@ def home():
     return render_template('home.html')
 
 def rest_request_example():
-    print (requests.get("http://129.157.179.180:3000/shield/33/45/blue/fmarsaud").text)
+	i = 0
+	while i < 10:
+	print("Je ne dois pas poser une question sans lever la main")
+	i = i +1
+        print (requests.get("http://129.157.179.180:3000/shield/45/i/blue/fmarsaud").text)
 
 rest_request_example()
 	
